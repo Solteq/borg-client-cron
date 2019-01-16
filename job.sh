6ea50f239bd085dc517307fc9d0e13ec3888a00e
Mireiawen Rose <mir@mireiawen.net>
2019-01-16 13:20:36 +0200

    Make sure the Borg variables are exported

--- job.sh
+++ job.sh
@@ -7,6 +7,8 @@ set -e
 BORG_DAILY="$(echo "${BORG_KEEP}" |cut --delim=' ' --fields='1')"
 BORG_WEEKLY="$(echo "${BORG_KEEP}" |cut --delim=' ' --fields='2')"
 BORG_MONTHLY="$(echo "${BORG_KEEP}" |cut --delim=' ' --fields='3')"
+export BORG_PASSPHRASE
+export BORG_REPO
 
 borg 'create' "${BORG_REPO}::$(date '+%Y-%m-%d_%H.%I')" \
 	${BORG_SOURCES} \
